59e5d171be56ac7388583ee8ca0a145ecb5a9530
jupterorbita <john.williams@outlook.com>
2018-07-02 11:57:24 -0700

    done

--- suits.playground/Contents.swift
+++ suits.playground/Contents.swift
@@ -1,10 +1,22 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//Our result should look something like the following:
+//["Clubs": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], "Diamonds": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], "Hearts": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], "Spades": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]]
+
 
 let suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 var deckOfCards = [String: [Int]]()
+
+
+
+
 // your code here
+print(deckOfCards)
+
+for s in 0...suits.count-1 {
+    deckOfCards[suits[s]] = cards
+}
 print(deckOfCards)
 
